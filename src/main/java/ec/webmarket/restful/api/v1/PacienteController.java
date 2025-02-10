5ab24d195e88f7b05782aaab0d6b6282b35011bb
AxelJhostin <axelhernan69@gmail.com>
2025-02-10 15:25:30 -0500

    Add paciente and cita

---
+++ src/main/java/ec/webmarket/restful/api/v1/PacienteController.java
@@ -0,0 +1,27 @@
+package ec.webmarket.restful.api.v1;
+
+import ec.webmarket.restful.dto.v1.PacienteDTO;
+import ec.webmarket.restful.service.crud.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteService service;
+
+    @PostMapping
+    public ResponseEntity<PacienteDTO> create(@RequestBody PacienteDTO dto) {
+        return ResponseEntity.ok(service.create(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PacienteDTO>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+}
---
+++ src/main/java/ec/webmarket/restful/dto/v1/PacienteDTO.java
@@ -0,0 +1,15 @@
+package ec.webmarket.restful.dto.v1;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class PacienteDTO {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String email;
+    private String telefono;
+    private LocalDate fechaRegistro;
+}
